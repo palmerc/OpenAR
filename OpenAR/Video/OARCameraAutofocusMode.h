5356ac5196f57a96e569b98cea846460642a09d0
Cameron Lowell Palmer <cameron.palmer@gmail.com>
2015-04-09 19:42:58 +0200

    Add two new enums for auto focus and image format of the camera

---
+++ OpenAR/Video/OARCameraAutofocusMode.h
@@ -0,0 +1,20 @@
+//
+//  OARCameraAutofocusMode.h
+//  OpenAR
+//
+//  Created by Cameron Palmer on 03.04.15.
+//  Copyright (c) 2015 OAR. All rights reserved.
+//
+
+#ifndef OpenAR_OARCameraAutofocusMode_h
+#define OpenAR_OARCameraAutofocusMode_h
+
+typedef enum {
+    OARCameraFocusModeDefault,
+    OARCameraFocusModeTriggerAuto,
+    OARCameraFocusModeContinuousAuto,
+    OARCameraFocusModeInfinity,
+    OARCameraFocusModeMacro
+} OARCameraAutofocusMode;
+
+#endif
---
+++ OpenAR/Video/OARCameraImageFormat.h
@@ -0,0 +1,17 @@
+//
+//  OARCameraImageFormat.h
+//  OpenAR
+//
+//  Created by Cameron Palmer on 03.04.15.
+//  Copyright (c) 2015 OAR. All rights reserved.
+//
+
+#ifndef OpenAR_OARCameraImageFormat_h
+#define OpenAR_OARCameraImageFormat_h
+
+typedef enum {
+    OARCameraImageFormatGrayscale = 1 << 0,
+    OARCameraImageFormatRGB       = 1 << 1
+} OARCameraImageFormat;
+
+#endif
