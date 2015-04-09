75c2dbf7533dab1e2f4b56e94107c1769b84d707
Cameron Lowell Palmer <cameron.palmer@gmail.com>
2015-04-09 19:42:22 +0200

    Add the marker result object to transport framemarker and pose

---
+++ OpenAR/Marker Detection/OARMarkerDetectorResult.h
@@ -0,0 +1,13 @@
+//
+//  OARMarkerDetectorResult.h
+//  OpenAR
+//
+//  Created by Cameron Palmer on 09.04.15.
+//  Copyright (c) 2015 OAR. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OARMarkerDetectorResult : NSObject
+
+@end
---
+++ OpenAR/Marker Detection/OARMarkerDetectorResult.m
@@ -0,0 +1,13 @@
+//
+//  OARMarkerDetectorResult.m
+//  OpenAR
+//
+//  Created by Cameron Palmer on 09.04.15.
+//  Copyright (c) 2015 OAR. All rights reserved.
+//
+
+#import "OARMarkerDetectorResult.h"
+
+@implementation OARMarkerDetectorResult
+
+@end
